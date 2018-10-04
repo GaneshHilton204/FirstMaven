d552f86a6ab1359bfb2879b6eea3782fec8ee54b
ganesh <ganesh.prasannamuthineni.contractor@hilton.com>
2018-10-04 13:51:28 -0500

    added a class to say hello

---
+++ src/test/java/org/jenkins/SayHello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.jenkins;
+
+/**
+ * @author ganesh
+ *
+ */
+public class SayHello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello.");
+	}
+
+}
